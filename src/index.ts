4bba66a723cdeb89f8638e90639f338100bef774
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2018-03-17 13:37:39 -0400

    Default css

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 
+var defaultCss = document.createElement('style')
+defaultCss.type = 'text/css'
+defaultCss.innerHTML = `
+* {
+  margin:0%;
+  padding: 0%;
+}
+canvas {
+  display: block;
+}
+`
+document.body.appendChild(defaultCss)
+
 import Scene from './Scene'
 import Render from './Render'
 import Sprite from './Sprite'
