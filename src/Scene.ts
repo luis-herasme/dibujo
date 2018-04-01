665791ba39626550b2cee2586851fc2218f8ebfb
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2018-04-01 14:04:24 -0400

    Wait for images to load before render

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,6 +1,8 @@
-import Render   from './Render'
-import Graphic  from './graphics/Graphic'
-import Camera   from './Camera'
+import Render from './Render'
+import Graphic from './graphics/Graphic'
+import Camera from './Camera'
+import Picture from './graphics/Picture'
+import Video from './graphics/Video'
 
 /**
  * This is the detail about the constructor
@@ -10,17 +12,30 @@ import Camera   from './Camera'
  */
 class Scene {
 
-  public context          : CanvasRenderingContext2D
-  public camera           : Camera
-  public renderer         : Render
-  public childs           : Array<Graphic> = []
-  public frameRate        : number         = 0
-  public interval         : any
+  public context: CanvasRenderingContext2D
+  public camera: Camera
+  public renderer: Render
+  public childs: Array<any> = []
+  public frameRate: number = 0
+  public ready: boolean = false
+  public interval: any
 
-  constructor (renderer: Render) {
+  constructor(renderer: Render) {
     this.renderer = renderer
-    this.context  = this.renderer.context
-    this.camera   = new Camera(this.context)
+    this.context = this.renderer.context
+    this.camera = new Camera(this.context)
+  }
+
+  dataLoaded(): boolean {
+    // Gets all the pictures and videos from the scene.
+    const elements = this.childs.filter((child) => {
+      if (child instanceof Picture) return true
+      if (child instanceof Video) return true
+      return false
+    })
+
+    // If there is an image that is not loaded returns false
+    return !elements.some((element) => !element.ready)
   }
 
   /**
@@ -28,7 +43,7 @@ class Scene {
    * @param {Graphic} element any graphic object
    * @returns {void}
    */
-  add (element: Graphic): void {
+  add(element: Graphic): void {
     element.context = this.context
     this.childs.push(element)
     this.organizeByZindex()
@@ -39,14 +54,17 @@ class Scene {
    * @param {Graphic} element any graphic object
    * @returns {void}
    */
-  remove (element: Graphic): void {
-    this.childs.splice(this.childs.indexOf(element), 1)
+  remove(element: Graphic): void {
+    let index = this.childs.indexOf(element)
+    if (index >= 0) {
+      this.childs.splice(index, 1)
+    }
   }
 
   /**
    * This method clears the screen
    */
-  clearScreen (): void {
+  clearScreen(): void {
     this.context.save()
     this.context.setTransform(1, 0, 0, 1, 0, 0)
     this.context.clearRect(0, 0, window.innerWidth, window.innerHeight)
@@ -57,12 +75,12 @@ class Scene {
    * This method enables or disables the image smoothing
    * @param {boolean} state enable or disable
    */
-  smoth (state: boolean): void {
+  smoth(state: boolean): void {
     if (this.context.imageSmoothingEnabled) {
       this.context.imageSmoothingEnabled = state
-    } else if (this.context.mozImageSmoothingEnabled) { 
+    } else if (this.context.mozImageSmoothingEnabled) {
       this.context.mozImageSmoothingEnabled = state
-    } else if (this.context.webkitImageSmoothingEnabled) { 
+    } else if (this.context.webkitImageSmoothingEnabled) {
       this.context.webkitImageSmoothingEnabled = state
     }
   }
@@ -70,31 +88,39 @@ class Scene {
   /**
    * This method organizes the childs of the scene by their property z_indez
    */
-  organizeByZindex (): void {
-    this.childs.sort((a,b) => a.z_index - b.z_index)
+  organizeByZindex(): void {
+    this.childs.sort((a, b) => a.z_index - b.z_index)
   }
 
   /**
    * This method renders the screne ultil you call stopAutoRender
    */
-  autoRender (): void {
-    this.interval = setInterval(() => this.render(), this.frameRate)
+  autoRender(func?: Function): void {
+    this.interval = setInterval(() => {
+      if (func) func()
+      this.render()
+    }, this.frameRate)
   }
 
   /**
    * This method stops autoRender
    */
-  stopAutoRender (): void {
+  stopAutoRender(): void {
     clearInterval(this.interval)
   }
 
   /**
    * This method renders the screne
    */
-  render (): void {
-    this.clearScreen()
-    this.childs.forEach(child => child.render())
-    this.camera.update()
+  render(): void {
+    if (!this.dataLoaded()) {
+      console.info('Waiting for images to load...')
+      setTimeout(this.render.bind(this), 100)
+    } else {
+      this.clearScreen()
+      this.childs.forEach(child => child.render())
+      this.camera.update()
+    }
   }
 }
 
