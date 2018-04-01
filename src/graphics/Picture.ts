8ce55e8882f73ddb2400c852d7093980b98c99a9
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2018-04-01 14:09:04 -0400

    Picture filter remove color wait to load

--- src/graphics/Picture.ts
+++ src/graphics/Picture.ts
@@ -1,29 +1,114 @@
 import Graphic from './Graphic'
+import Vector from '../Vector';
 
-export default class Picture extends Graphic {
+class Picture extends Graphic {
   public width: number
   public height: number
   public opacity: number
   public image: HTMLImageElement
+  public angle: number
   public type: string = "img"
+  public data: any
+  public ready: boolean = false
+  public todo: Array<Function> = []
+  public params: Array<any> = []
+
 
   constructor(data: any) {
     super(data)
     this.image = new Image()
     this.image.src = data.src
-    this.opacity = data.opacity ? data.opacity: 1
-    this.width = data.width ? data.width : 100
-    this.height = data.height ? data.height : 100
+
+    this.image.addEventListener('load', () => {
+      this.ready = true
+      console.info(`Image ${data.src} correctly loaded.`)
+      console.log(this.todo)
+      console.log(this.params)
+      this.todo.forEach((met, i) => {
+        met(...this.params[i])
+      })
+    })
+
+    this.image.addEventListener('error', () => {
+      console.error('Error loading the image...')
+    })
+
+    this.angle = data.angle ? data.angle : 0
+    this.opacity = data.opacity ? data.opacity : 1
+    this.width = data.width ? data.width : this.image.width
+    this.height = data.height ? data.height : this.image.height
   }
 
-  onClick(func: Function): void { }
+  filter() {
+
+  }
+
+  getImageData() {
+    const canvas = document.createElement('canvas')
+    // console.log(canvas)
+    document.body.appendChild(canvas)
+    const context = canvas.getContext('2d')
+    canvas.width = this.width
+    canvas.height = this.height
+    context.drawImage(this.image, 0, 0, this.width, this.height)
+     document.body.removeChild(canvas)
+    return context.getImageData(0, 0, this.width, this.height)
+  }
+
+  removeColor(color: Array<number>) {
+    if (this.ready) {
+      let image
+      if (!this.data) {
+        image = this.getImageData()
+      } else {
+        image = this.data
+      }
+
+      for (let pixel = 0; pixel < image.data.length; pixel += 4) {
+        if (image.data[pixel] === color[0] && image.data[pixel + 1] === color[1] && image.data[pixel + 2] === color[2]) {
+          image.data[pixel + 3] = 0
+        }
+      }
+
+      this.data = image
+      this.render = this.renderData
+    } else {
+      this.todo.push(this.removeColor.bind(this))
+      this.params.push([color])
+    }
+  }
+
+  onClick(func: Function): void {
+
+  }
+  realPosition() {
+    return new Vector(this.position.x - (this.anchor.x * this.width), this.position.y - (this.anchor.y * this.height))
+  }
+
+  renderData(): void {
+  //  console.log('here')
+    //console.log(this.data)
+    this.context.beginPath()
+    this.context.save()
+    // this.context.translate(this.position.x -(this.anchor.x *  this.width), this.position.y -(this.anchor.y *  this.height))
+    //this.context.rotate(this.angle)
+    // this.context.globalAlpha = this.opacity
+    // console.log(this.data)
+    this.context.putImageData(this.data, this.position.x -(this.anchor.x *  this.width), this.position.y -(this.anchor.y *  this.height))// -(this.anchor.x *  this.width), -(this.anchor.y *  this.height), this.position.x, this.position.y, this.width, this.height)
+    // this.context.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    this.context.restore()
+  }
 
   render(): void {
     this.context.beginPath()
     this.context.save()
+    this.context.translate(this.position.x, this.position.y)
+    this.context.rotate(this.angle)
     this.context.globalAlpha = this.opacity
-    this.context.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    this.context.drawImage(this.image, -(this.anchor.x * this.width), -(this.anchor.y * this.height), this.width, this.height)
+    // this.context.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
     this.context.restore()
   }
 }
 
+export default Picture
