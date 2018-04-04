174ac279efaa1c7ae2fd93fdd0b78ff5cb83739a
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2018-04-04 14:38:02 -0400

    Text config

--- src/graphics/Text.ts
+++ src/graphics/Text.ts
@@ -1,20 +1,35 @@
 import Graphic from './Graphic'
 
+enum weights {
+  lighter = 'lighter',
+  normal = 'normal',
+  bolder = 'bolder',
+  bold = 'bold'
+}
+
 class Text extends Graphic {
   public content: string
   public stroke: boolean
-  public style: any
   public type: string = "text"
+  public style: any
+
+  public weight: weights
+  public family: string
+  public size: number
+  public fontConfig: string = ''
 
   constructor(configuration: any) {
     super(configuration)
-    this.style = configuration.style ? configuration.style : {}
+    this.weight = configuration.weight ? configuration.weight : weights.normal
+    this.size = configuration.size ? configuration.size : 12
+    this.family = configuration.family ? configuration.family : 'Arial'
     this.content = configuration.content ? configuration.content : ''
     this.stroke = configuration.stroke ? configuration.stroke : false
   }
 
   render(): void {
-    this.setStyle(this.style)
+    this.context.font(`${this.weight} ${this.size}px ${this.family}`)
+
     if (this.stroke) {
       this.context.strokeText(this.content, this.position.x, this.position.y)
     }
